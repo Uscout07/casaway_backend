cab637c8d985fe7583fca75eb641b1c09c7bfa0c
Uscout07 <pantudit007@gmail.com>
2025-08-09 00:33:35 +0530

    aws

--- src/routes/speedTest.routes.ts
+++ src/routes/speedTest.routes.ts
@@ -6,10 +6,10 @@ const router = Router();
 
 // Configure multer for memory storage for this route
 const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
+// const upload = multer({ storage: storage });
 
-// Define the upload speed test route
-// Use upload.single('speedTestFile') middleware to handle a single file upload
-router.post('/upload', upload.single('speedTestFile'), uploadSpeedTest);
+// // Define the upload speed test route
+// // Use upload.single('speedTestFile') middleware to handle a single file upload
+// router.post('/upload', upload.single('speedTestFile'), uploadSpeedTest);
 
 export default router;
